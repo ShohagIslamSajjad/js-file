c4ff261de544ce79c92d045993a4a659ad3602d7
Shohag Islam Sajjad <73532354+ShohagIslamSajjad@users.noreply.github.com>
2020-12-15 18:31:39 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,65 @@
+
+const storeBody = document.querySelector(".storeBody")
+const html = `
+<div style="background-color:#6200EE;">
+
+<div class="container">
+  <div>
+  <div class="div1 p-2 ">
+  
+    <span class="mx-n5">
+
+
+   <input type="search" placeholder="Search..." name="search">
+   <button class="btn btn-warning"><b style="color:yellow;">Search</b></button>
+
+
+    </span>
+   <a class="text-warning" href="index.html"><b><i class="fas fa-home"></i>Home</b></a>
+
+  <a class="text-warning" href="login.html"><b><i class="fas fa-user-plus"></i>Login</b></a>
+  <a class="text-warning" href=""><b><i class="fas fa-sign-in-alt"></i>.SignUp</b></a>
+  <a class="text-warning" href="#"><b><i class="fas fa-cart-plus"></i>-Card</b></a>
+
+
+  </div>
+
+  </div>
+</div>
+
+</div>
+<div class="bg-warning pb-0">
+<div class=" p-5">
+<img style="height:400px" src="img/img1.jpg" class="img-thumbnail w-100" alt="Product Photo">
+</div>
+<div style="background-color: #6200EE;">
+  <hr class="mt-2" style="height: 5px;background-color: wheat;">
+
+  <div class="container">
+    <div>
+  
+
+<div class="row div-row">
+ 
+
+</div>
+
+
+    </div>
+  </div>
+  
+  
+  </div>
+</div>
+
+
+
+
+<div class="bg-dark mb-2"><br><br></div>  `
+storeBody,innerHTML= html
+
+
+
+
+
+
